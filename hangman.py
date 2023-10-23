a53d87a73d51f22ccb889bb1d9907de18417fe79
Erick <flpasana@outlook.com>
2023-10-23 20:13:52 +0800

    Oct 23
    Upload

---
+++ hangman.py
@@ -0,0 +1,74 @@
+import random
+import mywords
+
+
+print(mywords.logo)
+# word_list = ["aardvark", "baboon", "camel"]
+word_list = mywords.mywords
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word_list.
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word_list.
+
+chosen_word = random.choice(word_list)
+chosen_word_list = list(chosen_word)
+
+word_length = len(chosen_word_list)
+total_chances = 0
+missed_guess = 0
+# print(chosen_word_list)
+# print(word_length)
+display = ["_" for i in range(word_length)]
+guessed_letters = 0
+guessed_letters_list = []
+# for i in range(len(chosen_word_list)):
+#     display += "_"
+print(display)
+print(word_length)
+
+while missed_guess < 6 and "_" in display:
+    user_guess = input("Guess a letter: ").lower()
+    guessed_letters_list += user_guess
+    # if user_guess in display:
+
+    if user_guess in display:
+        print("You already guessed it. You missed!")
+        missed_guess += 1
+        print(f"Your current status: {display}")
+        print(f"Miss: {missed_guess} Chances Left: {10 - missed_guess}")
+        continue
+        # break
+    if user_guess in chosen_word:
+        for n in range(word_length):
+            if user_guess == chosen_word_list[n]:
+                display[n] = user_guess
+                # guessed_letters += 1
+                print("correct")
+                # continue
+                # break
+            else:
+                # missed_guess += 1
+                # print("Wrong")
+                # print(display)
+                continue
+                # break
+    else:
+        missed_guess += 1
+        print("You got it wrong this time.")
+    print(mywords.stages[6-missed_guess])
+    print(f"Your current status: {' '.join(display)}")
+    print(f"Your Guessed list: {guessed_letters_list}")
+    print(f"Miss: {missed_guess} Chances Left: {6 - missed_guess}")
+if "_" not in display:
+    print(display)
+    print(f"The hidden word is {chosen_word}. You Won!!!")
+else:
+    print(f"The hidden word is {chosen_word}. You Lost!!!")
+print("Game Over!!!")
+    # print(input("What's your first guess?"))
+
+
+
+
---
+++ mywords.py
@@ -0,0 +1,68 @@
+mywords = ['punch', 'rock', 'wriggle', 'calculator', 'pathetic', 'songs', 'uneven', 'shocking', 'brave', 'event', 'annoying', 'discovery', 'plucky', 'holiday', 'cake', 'call', 'disturbed', 'tooth', 'roasted', 'exultant', 'queue', 'exclusive', 'unkempt', 'boundless', 'tight', 'shake', 'elastic', 'extra-small', 'callous', 'prefer', 'hot', 'mammoth', 'knowledgeable', 'boring', 'mellow', 'appreciate', 'rapid', 'cultured', 'obsolete', 'attract', 'upbeat', 'fantastic', 'notebook', 'turkey', 'piquant', 'follow', 'soggy', 'caption', 'flat', 'giraffe', 'left', 'spotless', 'easy', 'oven', 'development', 'kaput', 'instinctive', 'credit', 'blue-eyed', 'subtract', 'unite', 'prick', 'apologise', 'borrow', 'idiotic', 'check', 'push', 'loose', 'tax', 'food', 'yellow', 'pastoral', 'grin', 'enchanting', 'materialistic', 'handsome', 'toothsome', 'stain', 'actor', 'sheet', 'accurate', 'kindly', 'delicate', 'mixed', 'picayune', 'add', 'downtown', 'resolute', 'attack', 'wretched', 'girls', 'anxious', 'mix', 'linen', 'lean', 'blushing', 'fluffy', 'nostalgic', 'gate', 'malicious', 'nut', 'pickle', 'handsomely', 'amused', 'tart', 'quicksand', 'room', 'chunky', 'women', 'rabbit', 'unsuitable', 'nervous', 'penitent', 'pail', 'enter', 'earth', 'basket', 'overflow', 'swing', 'insidious', 'exercise', 'fix', 'distinct', 'pie', 'relax', 'summer', 'continue', 'pull', 'male', 'mute', 'numerous', 'influence', 'wicked', 'brake', 'expert', 'hurt', 'tug', 'free', 'hunt', 'rake', 'collect', 'playground', 'noisy', 'hapless', 'death', 'wasteful', 'beautiful', 'inexpensive', 'behave', 'bucket', 'eyes', 'pest', 'examine', 'industrious', 'marvelous', 'dinner', 'horrible', 'shiny', 'makeshift', 'hammer', 'wander', 'wistful', 'march', 'tame', 'pack', 'arithmetic', 'return', 'sugar', 'brick', 'permissible', 'crazy', 'faulty', 'board', 'used', 'blade', 'jellyfish', 'route', 'meddle', 'stir', 'control', 'guiltless', 'cup', 'bawdy', 'irate', 'join', 'classy', 'lunch', 'pray', 'sleepy', 'outgoing', 'space', 'lie', 'far-flung', 'chase', 'spring', 'poor', 'barbarous', 'pumped', 'romantic', 'hideous', 'camp', 'stay', 'dad', 'serious', 'abrupt', 'weight', 'circle', 'huge', 'illustrious', 'salty', 'horn', 'beam', 'rotten', 'look', 'amusement', 'measly', 'quill', 'front', 'sort', 'ruddy', 'idea', 'stranger', 'lamentable', 'company', 'icky', 'alike', 'second', 'fallacious', 'object', 'animal', 'cowardly', 'greasy', 'alive', 'sad', 'muscle', 'lace', 'thing', 'ruin', 'loud', 'naive', 'conscious', 'fang', 'fierce', 'superb', 'possessive', 'great', 'signal', 'committee', 'hateful', 'connection', 'church', 'apathetic', 'song', 'meal', 'deep', 'obsequious', 'complete', 'deliver', 'turn', 'snotty', 'breakable', 'mouth', 'pricey', 'wet', 'letter', 'scare', 'premium', 'sail', 'precious', 'tranquil', 'cattle', 'moan', 'attach', 'dizzy', 'lunchroom', 'sophisticated', 'art', 'plain', 'houses', 'box', 'healthy', 'interfere', 'ground', 'reject', 'garrulous', 'axiomatic', 'tacit', 'queen', 'twist', 'abiding', 'gaze', 'thirsty', 'special', 'fabulous', 'crowd', 'vivacious', 'governor', 'diligent', 'number', 'celery', 'interrupt', 'side', 'guarantee', 'arrogant', 'past', 'oil', 'button', 'two', 'delight', 'partner', 'big', 'ratty', 'destroy', 'raspy', 'whisper', 'fact', 'squeamish', 'shut', 'near', 'servant', 'probable', 'juvenile', 'fearful', 'likeable', 'rifle', 'zesty', 'subdued', 'treatment', 'guttural', 'messy', 'lettuce', 'closed', 'even', 'show', 'seal', 'bedroom', 'bomb', 'silky', 'mindless', 'flock', 'paper', 'bashful', 'cub', 'color', 'argue', 'shelter', 'hushed', 'next', 'groan', 'dime', 'scribble', 'scrape', 'hilarious', 'didactic', 'letters', 'medical', 'found', 'trite', 'pot', 'wrap', 'aggressive', 'sharp', 'pour', 'debonair', 'level', 'unlock', 'bite', 'unique', 'hole', 'invincible', 'half', 'zealous', 'adaptable', 'marry', 'lip', 'channel', 'instruct', 'sound', 'airport', 'average', 'shade', 'provide', 'practice', 'successful', 'flesh', 'languid', 'eight', 'woman', 'dependent', 'stare', 'gratis', 'rabid', 'adamant', 'painful', 'ill-informed', 'sense', 'border', 'neat', 'lacking', 'hall', 'minor', 'thrill', 'concern', 'ugly', 'impulse', 'tidy', 'gabby', 'wave', 'nappy', 'embarrassed', 'expensive', 'defective', 'racial', 'punish', 'good', 'please', 'ignore', 'bumpy', 'obeisant', 'quiver', 'trust', 'cure', 'jump', 'tumble', 'veil', 'desert', 'terrible', 'treat', 'slap', 'lyrical', 'white', 'weary', 'complain', 'smash', 'snobbish', 'unruly', 'promise', 'versed', 'cakes', 'communicate', 'heal', 'stroke', 'snatch', 'pets', 'curly', 'waggish', 'capable', 'night', 'honey', 'crooked', 'wooden', 'meat', 'enthusiastic', 'festive', 'branch', 'drip', 'drink', 'profuse', 'crook', 'grateful', 'bathe', 'creator', 'hand', 'bear', 'filthy', 'crime', 'oatmeal', 'mailbox', 'deeply', 'close', 'tow', 'skin', 'party', 'aftermath', 'right', 'reply', 'coil', 'bee', 'obnoxious', 'lively', 'throne', 'psychedelic', 'roll', 'nod', 'apparatus', 'spoil', 'boy', 'unhealthy', 'pigs', 'pinch', 'worried', 'pretty', 'reason', 'irritating', 'silver', 'adorable', 'dislike', 'cough', 'selfish', 'baby', 'strong', 'steam', 'duck', 'peck', 'men', 'broad', 'pear', 'plough', 'plant', 'fowl', 'system', 'undesirable', 'release', 'loutish', 'rebel', 'ignorant', 'grape', 'robin', 'unknown', 'try', 'previous', 'bottle', 'juggle', 'engine', 'cracker', 'end', 'mushy', 'fortunate', 'lonely', 'dull', 'dream', 'pleasant', 'spark', 'form', 'furniture', 'wax', 'preserve', 'knowing', 'deafening', 'sassy', 'last', 'proud', 'dinosaurs', 'wise', 'thumb', 'statuesque', 'peel', 'snake', 'flower', 'miss', 'pan', 'divergent', 'relation', 'tie', 'invite', 'abandoned', 'lackadaisical', 'questionable', 'chew', 'puzzled', 'hill', 'carriage', 'tip', 'territory', 'twig', 'moon', 'splendid', 'harm', 'typical', 'overjoyed', 'excite', 'black', 'encouraging', 'careful', 'exotic', 'fine', 'file', 'switch', 'chief', 'grab', 'list', 'planes', 'fluttering', 'market', 'first', 'alert', 'incompetent', 'condemned', 'ahead', 'marble', 'absent', 'quarter', 'succeed', 'lock', 'trick', 'living', 'unaccountable', 'visitor', 'start', 'shaky', 'pushy', 'self', 'tangible', 'hover', 'wall', 'card', 'range', 'own', 'fool', 'juicy', 'fertile', 'gather', 'kittens', 'bizarre', 'post', 'earthquake', 'draconian', 'uptight', 'ubiquitous', 'extend', 'electric', 'acceptable', 'spicy', 'brown', 'gigantic', 'breezy', 'dam', 'vengeful', 'comb', 'tremendous', 'cable', 'detect', 'bake', 'fax', 'launch', 'glue', 'sparkle', 'elegant', 'glistening', 'boorish', 'dirty', 'impolite', 'correct', 'income', 'afterthought', 'historical', 'government', 'harmony', 'thank', 'wobble', 'birth', 'sip', 'bulb', 'tin', 'touch', 'unequal', 'supreme', 'elated', 'trucks', 'harsh', 'respect', 'rain', 'cloudy', 'cushion', 'sign', 'unwritten', 'afford', 'blush', 'highfalutin', 'hug', 'awake', 'trashy', 'egg', 'analyze', 'meek', 'envious', 'ski', 'view', 'fireman', 'strange', 'woozy', 'star', 'scratch', 'ill-fated', 'deserted', 'humdrum', 'trains', 'cook', 'decorate', 'jam', 'zebra', 'children', 'torpid', 'rainstorm', 'fretful', 'depend', 'dead', 'six', 'sun', 'history', 'ragged', 'language', 'hard', 'wrong', 'heavy', 'striped', 'steadfast', 'tasty', 'sore', 'tongue', 'marked', 'explain', 'care', 'interest', 'useless', 'meaty', 'absurd', 'umbrella', 'profit', 'aware', 'pocket', 'teeth', 'married', 'butter', 'pleasure', 'wing', 'lucky', 'cave', 'ghost', 'feigned', 'ball', 'office', 'label', 'madly', 'sticks', 'bit', 'puny', 'wrestle', 'pig', 'aquatic', 'bustling', 'offbeat', 'wacky', 'old', 'test', 'unarmed', 'acrid', 'toad', 'scene', 'scarce', 'hour', 'flood', 'uncle', 'plot', 'wiggly', 'writing', 'move', 'ear', 'jagged', 'delirious', 'halting', 'foot', 'simple', 'legs', 'crack', 'innocent', 'ripe', 'allow', 'reproduce', 'earsplitting', 'tightfisted', 'direful', 'colorful', 'thoughtful', 'size', 'airplane', 'silent', 'cover', 'unusual', 'machine', 'bare', 'wry', 'military', 'day', 'muddle', 'agree', 'cellar', 'curved', 'giddy', 'prose', 'tense', 'appear', 'kettle', 'ticket', 'devilish', 'murder', 'chilly', 'leg', 'river', 'laughable', 'arrive', 'float', 'team', 'ten', 'rigid', 'match', 'second-hand', 'flap', 'house', 'disgusting', 'glorious', 'angle', 'lumber', 'handy', 'morning', 'misty', 'afraid', 'yoke', 'delightful', 'lake', 'skip', 'delicious', 'settle', 'cap', 'collar', 'internal', 'clever', 'chop', 'warn', 'robust', 'chalk', 'greedy', 'hungry', 'yam', 'swim', 'slave', 'order', 'elfin', 'root', 'basketball', 'repeat', 'perfect', 'recondite', 'nonstop', 'tender', 'impartial', 'waste', 'null', 'squeal', 'utopian', 'finicky', 'flippant', 'chess', 'ritzy', 'deadpan', 'womanly', 'haunt', 'horses', 'shave', 'metal', 'equable', 'empty', 'love', 'economic', 'nutty', 'door', 'inject', 'army', 'arch', 'flowery', 'gifted', 'theory', 'line', 'work', 'fish', 'behavior', 'magical', 'fetch', 'aback', 'possible', 'wren', 'snore', 'refuse', 'vegetable', 'expansion', 'excuse', 'cart', 'efficacious', 'craven', 'scandalous', 'wiry', 'maddening', 'nebulous', 'pine', 'overwrought', 'title', 'wild', 'offer', 'political', 'yard', 'outstanding', 'rare', 'abortive', 'gun', 'infamous', 'shame', 'mountain', 'weather', 'hope', 'compare', 'strap', 'dry', 'shop', 'vanish', 'befitting', 'abusive', 'damaging', 'hose', 'vacuous', 'dance', 'modern', 'want', 'perpetual', 'rhythm', 'reach', 'scatter', 'stick', 'floor', 'beneficial', 'dogs', 'naughty', 'vase', 'nimble', 'quack', 'safe', 'plan', 'superficial', 'guard', 'press', 'understood', 'carpenter', 'frogs', 'memory', 'grouchy', 'dispensable', 'dark', 'magnificent', 'acidic', 'moor', 'bike', 'cooing', 'sweltering', 'intend', 'gamy', 'slippery', 'melted', 'flash', 'pizzas', 'humor', 'pause', 'rich', 'page', 'functional', 'beg', 'addition', 'periodic', 'dashing', 'temporary', 'fry', 'high', 'snails', 'afternoon', 'load', 'substance', 'science', 'manage', 'stupendous', 'wealthy', 'victorious', 'charming', 'alcoholic', 'sigh', 'mother', 'confuse', 'permit', 'bad', 'replace', 'peep', 'powerful', 'cluttered', 'need', 'noise', 'helpless', 'gold', 'suppose', 'bag', 'freezing', 'magic', 'spurious', 'decide']
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''

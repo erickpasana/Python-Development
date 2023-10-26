bce2ea8de0e3ec9a15c2e9c3cad83cf216f25f27
Erick <flpasana@outlook.com>
2023-10-26 10:02:45 +0800

    oct 26 2

---
+++ calc.py
@@ -0,0 +1,19 @@
+from mywords import calc_logo
+
+logo = calc_logo
+
+def add(n1, n2):
+    result = n1 + n2
+    return result
+
+def subtract(n1, n2):
+    result = n1 - n2
+    return result
+
+def multiply(n1, n2):
+    result = n1 * n2
+    return result
+
+def divide(n1, n2):
+    result = n1/n2
+    return result

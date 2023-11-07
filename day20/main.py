dd0be9b54fd27a72c1b72803d5e71f4c5362aa3a
Erick <flpasana@outlook.com>
2023-11-07 20:05:42 +0800

    nov 7

--- day20/main.py
+++ day20/main.py
@@ -1,6 +1,9 @@
-from turtle import Screen
+from turtle import Turtle, Screen
+from turtle import clear
+from click import clear
 from time import sleep
 from snake import Snake
+from scoreboard import Scoreboard
 import sys
 sys.path.append(r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day21")
 from food import Food
@@ -15,6 +18,7 @@ screen.tracer(0)
 # TODO: Create a snake body
 my_snake = Snake()
 food = Food()
+score_board = Scoreboard()
 # screen.update()
 
 # TODO: Move the snake
@@ -23,7 +27,7 @@ screen.onkey(my_snake.left, 'Left')
 screen.onkey(my_snake.right, 'Right')
 screen.onkey(my_snake.up, 'Up')
 screen.onkey(my_snake.down, 'Down')
-screen.onkey(my_snake.down, 'Down')
+# screen.onkey(my_snake.down, 'Down')
 
 game_is_on = True
 while game_is_on:
@@ -33,8 +37,26 @@ while game_is_on:
     my_snake.move()
     if my_snake.head.distance(food) < 15:
         food.new_loc()
+        score_board.add_score()
+        my_snake.extend()
         print('nomnomnom')
+    if my_snake.head.xcor() > 295 or my_snake.head.ycor() > 295 or my_snake.head.xcor() < -295 or my_snake.head.ycor() < -295:
+        # screen.clear()
+        score_board.game_over()
+        # my_snake.clear()
+        # food.clear()
+        game_is_on = False
+    for seg in my_snake.body[2:]:
+        if my_snake.head.distance(seg) < 10:
+            game_is_on = False
 
+    # game_is_on = False for seg in my_snake.body[2:] if my_snake.head.distance(seg) < 10:
+
+
+
+
+# clear()
+score_board.game_over()
 
 
 
@@ -48,13 +70,6 @@ while game_is_on:
 # TODO: Detect collision with wall
 
 # TODO: Detect collision with body
-
-
-
-
-
-
-
 
 
 screen.exitonclick()

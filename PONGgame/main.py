860a5946c3074384bfeec07b0d803bd184069600
Erick <flpasana@outlook.com>
2023-11-08 17:25:45 +0800

    nov 8

--- PONGgame/main.py
+++ PONGgame/main.py
@@ -26,6 +26,7 @@ from turtle import Turtle, Screen
 from time import sleep
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import sys
 sys.path.append(r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day21")
 
@@ -40,33 +41,66 @@ screen.tracer(0)
 # screen.delay(0)
 
 # #TODO: Create Paddle class
+# paddle_R = Paddle(width, height)
+# paddle_L = Paddle(-width, height)
+# ball = Ball(width, height)
+# screen.update()
+
+
+
+# def play_pong():
+
+#TODO: Create Paddle class
 paddle_R = Paddle(width, height)
 paddle_L = Paddle(-width, height)
 ball = Ball(width, height)
-# screen.update()
+R_score = Scoreboard(width, height)
+L_score = Scoreboard(-width, height)
+screen.update()
 
 # #TODO: Move Paddle
 screen.listen()
 screen.onkeypress(paddle_R.go_up, 'Up')
 screen.onkeypress(paddle_R.go_down, 'Down')
-screen.onkeypress(paddle_L.go_up, 'w')
-screen.onkeypress(paddle_L.go_down, 's')
+screen.onkeypress(paddle_L.go_up, 'o')
+screen.onkeypress(paddle_L.go_down, 'l')
 # screen.update()
-
 game_is_on = True
 while game_is_on:
-    screen.update()
-    screen.tracer(1)
-    # sleep(3)
+    # screen.update()
+    # screen.tracer(1)
     ball.move()
+    sleep(0.15)
     screen.update()
 
+#TODO: Detect ball collision with walls
+    if ball.ycor() > height/2 - 20 or ball.ycor() < -height/2 + 30:
+        ball.bounce_y()
+
+#TODO: Detect ball collision with paddles
+    if ball.distance(paddle_R) < 30 and ball.xcor() > 20:
+    # if ball.distance(paddle_R) < 30 or ball.distance(paddle_L) < 30:
+        # print("Made Contact")
+        ball.bounce_x()
+    if ball.distance(paddle_L) < 30 and ball.xcor() < -20:
+    # if ball.distance(paddle_L) < 30:
+        # print("Made Contact")
+        ball.bounce_x()
+
+#TODO: Paddle misses; out of bounds
+    if ball.xcor() > width/2 - 20:
+        L_score.add_score()
+        ball.miss_reverse()
+
+    if ball.xcor() < -width/2 + 20:
+        R_score.add_score()
+        ball.miss_reverse()
     # sleep(3)
     # game_is_on = False
 
 
 
-
+# play_pong()
 
 
 
@@ -77,9 +111,9 @@ screen.exitonclick()
 # Paddles class
 # Ball class
 
-#TODO: Create ball and it' moves
-#TODO: Detect ball collision with walls
-#TODO: Paddle misses
+#TODO: Create ball an
+# 
+# d it' moves
 #TODO: Keep score
 
 

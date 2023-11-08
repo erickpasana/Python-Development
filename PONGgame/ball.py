9788dfe5c9a64f239945278fbe75dece4565ffcb
Erick <flpasana@outlook.com>
2023-11-08 17:25:35 +0800

    nov 8

--- PONGgame/ball.py
+++ PONGgame/ball.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from time import sleep
 
 
 class Ball(Turtle):
@@ -13,6 +14,23 @@ class Ball(Turtle):
         # self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(0, 0)
+        self.x_move = 15
+        self.y_move = 15
 
     def move(self):
-        self.goto(self.x/2-20, self.y/2-10)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        # if self.ycor() > self.y/2-20:
+        #     new_y = self.ycor() - 10
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def miss_reverse(self):
+        self.goto(0, 0)
+        sleep(1)
+        self.bounce_x()

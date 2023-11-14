303e4c3ae991463de2ca2c018670df5a53a4953e
Erick <flpasana@outlook.com>
2023-11-14 18:46:01 +0800

    pwd

--- day29/password-manager-start/main.py
+++ day29/password-manager-start/main.py
@@ -13,15 +13,16 @@ window.title("My Password Manager")
 # window.geometry(f'{width}x{height}')
 window.minsize(width, height)
 window.config(padx = 20, pady = 20)
-window.config(padx=80, pady= 20,) #bg=YELLOW
+window.config(padx=20, pady= 20,) #bg=YELLOW
 
 # canvas = Canvas(window)
 # canvas.create_image(img)
-canvas = Canvas()   #bg=YELLOW, highlightthickness=0
+canvas = Canvas(width=width, height=height)   #bg=YELLOW, highlightthickness=0
 img = PhotoImage(file='logo.png')
-canvas.create_image(100, 130, image=img)
+canvas.create_image(100, 100, image=img)
 # canvas_txt = canvas.create_text(100, 150, font=(FONT_NAME, 30, 'bold'), fill='white', text='00:00')
-canvas.grid(column=0, row= 1)
+canvas.pack()
+# canvas.grid(column=0, row= 1)
 
 
 

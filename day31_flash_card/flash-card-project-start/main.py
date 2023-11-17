cd641bb629c6e1581338c7c793709c1b51f53e4d
Erick <flpasana@outlook.com>
2023-11-17 18:30:33 +0800

    flash card

--- day31_flash_card/flash-card-project-start/main.py
+++ day31_flash_card/flash-card-project-start/main.py
@@ -11,16 +11,16 @@ HEIGHT = 526
 window = Tk()
 window.title("Flash Card")
 # window.geometry(f"{WIDTH}x{HEIGHT}")
-window.minsize(WIDTH, HEIGHT)
+# window.minsize(WIDTH, HEIGHT)
 window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)#
 
-flash_card = Canvas(width=WIDTH-100, height=HEIGHT-100, bg='red', highlightthickness=0)   #bg=YELLOW, width=WIDTH-100, height=HEIGHT-100, , HEIGHT-100,
-img = PhotoImage(file=r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\images\card_front.png")
-# flash_card.create_image(100, 100, image=img)
-flash_card.create_image(350, 200, image=img)#350, 200
-canvas_txt_country = flash_card.create_text(350, 150, font=(FONT_NAME, 20, 'italic'), fill='black', text='French')#'bold'
-canvas_txt_word = flash_card.create_text(350, 263, font=(FONT_NAME, 40, 'bold'), fill='black', text='Louvre')
-flash_card.grid(column=0, row= 0, columnspan=2)#,  sticky="nsew"
+flash_card = Canvas(width=WIDTH, height=HEIGHT, ) #bg='red',  #, width=WIDTH-100, height=HEIGHT-100, , HEIGHT-100,
+card_front_img = PhotoImage(file=r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\images\card_front.png")
+flash_card.create_image(400, 263, image=card_front_img)#350, 200
+canvas_txt_country = flash_card.create_text(400, 150, font=(FONT_NAME, 40, 'italic'), fill='black', text='Title')#'bold'
+canvas_txt_word = flash_card.create_text(400, 263, font=(FONT_NAME, 60, 'bold'), fill='black', text='Word')
+flash_card.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+flash_card.grid(column=0, row= 0, columnspan=2)#,  sticky="nsew" 
 
 #Buttoms
 # cross = Button()

a4d19c575a6ffec4571431ffde52852ec0be9131
Erick <flpasana@outlook.com>
2023-11-18 19:15:15 +0800

    Flash card

--- day31_flash_card/flash-card-project-start/main.py
+++ day31_flash_card/flash-card-project-start/main.py
@@ -9,50 +9,56 @@ BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
 WIDTH = 800
 HEIGHT = 526
-path = pathlib.Path(r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\data\french_words.csv")
-data = pd.read_csv(path)
-data_dict_list2 = data.to_dict('records')
-
+PATH = pathlib.Path(r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\data\french_words.csv")
+DATA = pd.read_csv(PATH)
+data_dict_list2 = DATA.to_dict('records')
+the_word_dict = random.choice(data_dict_list2)
+to_be_translated_country = DATA.columns[0]
+translation_country = DATA.columns[1]
+canvas_flash_card = True
 
 # ---------------------------- Functions ------------------------------- #
 # Read the data file with pandas. Function to read and display the words...
-def index():
-    return random.randint(0, len(data_dict_list2)-1)
 
 def create_flash_card():
+    global data_dict_list2, the_word_dict, canvas_flash_card, timer
+    window.after_cancel(timer)
+    flash_card.itemconfig(canvas_image, image=card_front_img)
+    flash_card.itemconfig(canvas_txt_country, text=to_be_translated_country, fill='black')
+    flash_card.itemconfig(canvas_txt_word, font=(FONT_NAME, 60, 'bold'), fill='black')
+    the_word_dict = random.choice(data_dict_list2)
     to_be_translated()
+    timer = window.after(3000, switch_canvas)
 
 def to_be_translated():
-    data_dict_list = data.to_dict()
-    base_words = data_dict_list['French']
-    base_word = data_dict_list2[index()]['French']
+    base_word = the_word_dict[to_be_translated_country]
     flash_card.itemconfig(canvas_txt_word, text=base_word)
-    print(index())
-    print(data_dict_list2[index()])
-    print(translation())
+    print(the_word_dict)
+    
 
-def translation():
-    # data = pd.read_csv(path)
-    # data_dict = {row.French: row.English for (index, row) in data.iterrows()}
-    translation = data_dict_list2[index()]['English']
+def switch_canvas():
+    translation = the_word_dict[translation_country]
+    flash_card.itemconfig(canvas_image, image=card_back_img)
+    flash_card.itemconfig(canvas_txt_country, text=translation_country, fill='white')
+    flash_card.itemconfig(canvas_txt_word, text=translation, fill='white')
+    print(the_word_dict)
     print(translation)
-    # print(index())
-    # print(data_dict_list2[index()])
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flash Card")
-# window.geometry(f"{WIDTH}x{HEIGHT}")
-# window.minsize(WIDTH, HEIGHT)
 window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)#
 
-flash_card = Canvas(width=WIDTH, height=HEIGHT, ) #bg='red',  #, width=WIDTH-100, height=HEIGHT-100, , HEIGHT-100,
+timer = window.after(3000, switch_canvas)
+
+flash_card = Canvas(width=WIDTH, height=HEIGHT, ) 
 card_front_img = PhotoImage(file=r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\images\card_front.png")
-flash_card.create_image(400, 263, image=card_front_img)#350, 200
-canvas_txt_country = flash_card.create_text(400, 150, font=(FONT_NAME, 40, 'italic'), fill='black', text='French')#'bold'
-canvas_txt_word = flash_card.create_text(400, 263, font=(FONT_NAME, 60, 'bold'), fill='black', text='Word')#canvas_txt_word
+card_back_img = PhotoImage(file=r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\images\card_back.png")
+canvas_image = flash_card.create_image(400, 263, image=card_front_img)
+canvas_txt_country = flash_card.create_text(400, 150, font=(FONT_NAME, 40, 'italic'), fill='black', text=f"Today's words are {to_be_translated_country}")
+canvas_txt_word = flash_card.create_text(400, 263, font=(FONT_NAME, 30, ), fill='black', text="Click any button to start")
+flash_card.grid(column=0, row=0, columnspan=2)
 flash_card.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-flash_card.grid(column=0, row= 0, columnspan=2)#,  sticky="nsew" 
 
 #Buttoms
 # cross = Button()
@@ -65,8 +71,37 @@ check_img = ImageTk.PhotoImage(Image.open(r"C:\Users\flpas\OneDrive - Connection
 check = Button(width=50, height=50, image=check_img, command=create_flash_card)#window
 check.grid(column=1, row=1)
 
-
-
+# job_id = window.after(3000, switch_canvas)
+# window.after(1000, switch_canvas)
 
 
 window.mainloop()
+
+
+
+
+
+
+# def switch_canvas():
+    # flash_card.pack_forget()  # Hide canvas1
+    # back_card.grid(column=0, row=0, columnspan=2)  # Show canvas2
+    # print(canvas_flash_card)
+    # window.after(3000, switch_back)
+    # flash_card.itemconfig(canvas_image, image=card_back_img)
+    # flash_card.itemconfig(canvas_txt_country, text=translation_country)
+    # translation()
+    # flash_card.create_image(400, 263, image=card_back_img)
+    # flash_card.itemconfig(canvas_txt_country,
+
+# def switch_back():
+#     back_card.pack_forget()  # Hide canvas2
+#     flash_card.grid(column=0, row=0, columnspan=2)  # Show canvas1 again
+    # print(canvas_flash_card)
+
+# back_card = Canvas(width=WIDTH, height=HEIGHT, )
+# card_back_img = PhotoImage(file=r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\images\card_back.png")
+# back_card.create_image(400, 263, image=card_back_img)
+# back_card_txt_country = back_card.create_text(400, 150, font=(FONT_NAME, 40, 'italic'), fill='white', text=translation_country)
+# back_card_txt_word = back_card.create_text(400, 263, font=(FONT_NAME, 30, ), fill='white', text="Click any button to start")
+# # back_card.grid(column=0, row=0, columnspan=2)
+# back_card.config(bg=BACKGROUND_COLOR, highlightthickness=0)

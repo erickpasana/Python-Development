617a1df22ada02ce72f6c95fe9dade3cd56c8a67
Erick <flpasana@outlook.com>
2023-11-20 15:07:08 +0800

    Day 32

---
+++ day32_sendEmail_manageDates/Birthday Wisher (Day 32) start/smtp_email.py
@@ -0,0 +1,42 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "omegakonstrukt@gmail.com"
+PWD = "hxtf fjvb aext gsvy"
+
+# connection = smtplib.SMTP("smtp.gmail.com", 587)
+# connection.starttls()
+# connection.login(user=MY_EMAIL, password=PWD)
+# connection.sendmail(from_addr=MY_EMAIL, to_addrs="laopasana@outlook.com", msg="Subject: Hello\n\nThis is the body of my test email.")
+# connection.close()
+
+#x--------------------------------- Constants ---------------------------------------x
+# MY_EMAIL = "omegakonstrukt@gmail.com"
+# PWD = "hxtf fjvb aext gsvy"
+# connection = smtplib.SMTP("smtp.gmail.com")
+#463xJUlY8hq3   "hxtf fjvb aext gsvy"
+#x------------------------------- Get Email Body -------------------------------------x
+
+
+#x--------------------------------- Send Email ---------------------------------------x
+now = dt.datetime.now()
+weekday = now.strftime("%A")
+# year = now.year
+# month = now.month
+# # weekday = now.weekday()
+
+if weekday == 'Monday':
+    index = random.randint(0, 102)
+    with open('quotes.txt', 'r') as file:
+        quotes = file.readlines()
+        quote = random.choice(quotes)
+        email_body = quote
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PWD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="laopasana@outlook.com", msg=f"Subject: Monday Inspirational Quote\n\n{quote}")
+
+    print(weekday)
+    print(email_body)

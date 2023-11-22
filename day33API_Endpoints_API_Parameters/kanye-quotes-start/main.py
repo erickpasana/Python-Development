49ca288d0e49a0e30627855b755e5bd3b342e610
Erick <flpasana@outlook.com>
2023-11-22 10:50:42 +0800

    Day 33

--- day33API_Endpoints_API_Parameters/kanye-quotes-start/main.py
+++ day33API_Endpoints_API_Parameters/kanye-quotes-start/main.py
@@ -1,36 +1,39 @@
 from tkinter import *
 import requests
-
-
-# def get_quote():
-#     API = "https://api.kanye.rest"
-#     response = requests.get(url=API)
-#     canvas.itemconfig(quote_text, text=response)
-#     response.raise_for_status()
+import json
 
 
 def get_quote():
     API = "https://api.kanye.rest"
+    response = requests.get(url=API)
+    response.raise_for_status()
+    data = response.json()
+    quote = data['quote']
+    canvas.itemconfig(quote_text, text=quote)
+
+
+# def get_quote():
+#     API = "https://api.kanye.rest"
     # Use a try-except block to handle errors
-    try:
+    # try:
         # Make a GET request to the API
-        response = requests.get(url=API)
+        # response = requests.get(url=API)
         # Raise an exception if the status code is not 200
-        response.raise_for_status()
+        # response.raise_for_status()
         # Get the data as a Python dictionary
-        data = response.json()
+        # data = response.json()
         # Get the quote as a string
-        quote = data['quote']
+        # quote = data['quote']
         # Update the text of the quote_text item on the canvas
-        canvas.itemconfig(quote_text, text=quote)
-    except requests.exceptions.RequestException as e:
+    #     canvas.itemconfig(quote_text, text=quote)
+    # except requests.exceptions.RequestException as e:
         # Print or display the HTTP error message
-        print(e)
-        canvas.itemconfig(quote_text, text="An HTTP error occurred.")
-    except json.decoder.JSONDecodeError as e:
+        # print(e)
+        # canvas.itemconfig(quote_text, text="An HTTP error occurred.")
+    # except json.decoder.JSONDecodeError as e:
         # Print or display the JSON error message
-        print(e)
-        canvas.itemconfig(quote_text, text="A JSON error occurred.")
+        # print(e)
+        # canvas.itemconfig(quote_text, text="A JSON error occurred.")
     #Write your code here.
 
 
@@ -42,7 +45,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"), fill="white")
+quote_text = canvas.create_text(150, 177, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
--- day33API_Endpoints_API_Parameters/main.py
+++ day33API_Endpoints_API_Parameters/main.py
@@ -1,9 +1,79 @@
 import requests
+import smtplib
 import datetime
+import pytz
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
-response.raise_for_status()
-data = response.json()
-data = data['timestamp']
-dt = datetime.datetime.fromtimestamp(data)
-print(dt)
+# 10.296758,124.789370
+MY_LAT = 10.29675
+MY_LNG = 124.78936
+API = 'https://api.sunrise-sunset.org/json'
+MY_EMAIL = "omegakonstrukt@gmail.com"
+PWD = "hxtf fjvb aext gsvy"
+
+PARAMETERS = {
+    'lat': MY_LAT,
+    'lng': MY_LNG,
+    'formatted': 0
+}
+
+def viewable_time():
+    #Determine if the ISS is at a viewable time
+    response = requests.get(url=API, params=PARAMETERS, )
+    response.raise_for_status()
+    data = response.json()
+    sunrise = data['results']['sunrise']
+    sunset = data['results']['sunset']
+    sunrise_dt = datetime.datetime.fromisoformat(sunrise)
+    sunset_dt = datetime.datetime.fromisoformat(sunset)
+    sunset_dt = sunset_dt.replace(tzinfo=pytz.timezone("Asia/Manila"))
+    utc_8 = datetime.timedelta(hours=8)
+    sunset_dt_8 = sunset_dt + utc_8
+    td = datetime.timedelta(hours=3)
+    tplus = sunset_dt_8 + td
+    time_now = datetime.datetime.now()
+    time_now = time_now.replace(tzinfo=pytz.timezone("Asia/Manila"))
+    
+    if time_now > sunset_dt_8 and time_now < tplus:
+        return True
+    else:
+        return False
+
+
+def ISS_view_loc():
+    response = requests.get(url='http://api.open-notify.org/iss-now.json')
+    response.raise_for_status()
+    data = response.json()
+    lat = float(data['iss_position']['latitude'])
+    lng = float(data['iss_position']['longitude'])
+    if lat > MY_LAT-3 and lat < MY_LAT+3 and lng > MY_LNG-3 and lng < MY_LNG+3:
+        return True
+    else:
+        return False
+
+def email_notification():
+    if viewable_time() == True and ISS_view_loc == True:
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PWD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs='laopasana@outlook.com', msg=f"Subject: Happy Birthday\n\n{letter_email(dict['name'])}")
+
+
+print(ISS_view_loc())
+print(viewable_time())
+
+
+
+# lat (float): Latitude in decimal degrees. Required.
+# lng (float): Longitude in decimal degrees. Required.
+# date (string): Date in YYYY-MM-DD format. Also accepts other date formats and even relative date formats. If not present, date defaults to current date. Optional.
+# callback (string): Callback function name for JSONP response. Optional.
+# formatted (integer): 0 or 1 (1 is default). Time values in response will be expressed following ISO 8601 and day_length will be expressed in seconds. Optional.
+
+# {
+#   "message": "success", 
+#   "timestamp": UNIX_TIME_STAMP, 
+#   "iss_position": {
+#     "latitude": CURRENT_LATITUDE, 
+#     "longitude": CURRENT_LONGITUDE
+#   }
+# }

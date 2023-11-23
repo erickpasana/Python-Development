5ae08b76e53505781f55a4544a5713ca6d4e9db6
Erick <flpasana@outlook.com>
2023-11-24 06:56:36 +0800

    Day 34

--- day17/quiz-game-final/ui.py
+++ day17/quiz-game-final/ui.py
@@ -11,6 +11,9 @@ HEIGHT = 400
 class QuizInterface:
 
     def __init__(self):
+        self.question_number = 0
+        self.score = 0
+        # self.question_list = q_list
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.configure(padx=20, pady=20, bg=THEME_COLOR)
@@ -23,20 +26,24 @@ class QuizInterface:
             self.question_bank.append(new_question)
         self.quiz = QuizBrain(self.question_bank)
 
-        self.q_card = Canvas(width=WIDTH, height=HEIGHT, )
-        q_in_qcard = self.q_card.create_text(100, 200, font=('Arial', 20, ), fill='black', text=self.quiz.next_question())
-        self.q_card.grid(column=0, row=2, columnspan=2)
+        self.q_card = Canvas(self.window, width=WIDTH, height=HEIGHT/2, )#
+        q_in_qcard = self.q_card.create_text(100, 100, font=('Arial', 20, ), fill='black', text='Hello')#self.quiz.next_question()
+        self.q_card.grid(column=0, row=1, columnspan=2)
 
         #Buttoms
-        # cross = Button()
-        # cross_img = ImageTk.PhotoImage(Image.open(r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\images\wrong.png"))
-        # cross = Button(width=50, height=50, image=cross_img, command=create_flash_card)#window
-        # cross.grid(column=0, row=1)
+        # self.cross = Button()
+        cross_img = PhotoImage(file=(r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day17\quiz-game-final\images\false.png"))
+        self.cross = Button(width=50, height=50, image=cross_img, )#command=create_flash_card
+        self.cross.grid(column=0, row=2)
 
         # # check = Button()
-        # check_img = ImageTk.PhotoImage(Image.open(r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day31_flash_card\flash-card-project-start\images\right.png"))
-        # check = Button(width=50, height=50, image=check_img, command=words_to_learn)#window
-        # check.grid(column=1, row=1)
+        check_img = PhotoImage(file=(r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development\day17\quiz-game-final\images\true.png"))
+        self.check = Button(width=50, height=50, image=check_img, )#command=words_to_learn
+        self.check.grid(column=1, row=2)
+
+        #Labels
+        self.score = Label(text="Score: 0", font=("Arial", 10, ), bg=THEME_COLOR)
+        self.score.grid(column=1, row=0, sticky=NE)#"bold"
 
 
 

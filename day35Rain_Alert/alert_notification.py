8cc37939a5d1bbfde67671a34ff6bb6e4ed31625
Erick <flpasana@outlook.com>
2023-11-30 13:49:08 +0800

    Day 35 Done

--- day35Rain_Alert/alert_notification.py
+++ day35Rain_Alert/alert_notification.py
@@ -1,5 +1,4 @@
 # Python Email/Message Sending
-
 import smtplib
 import requests
 import json
@@ -17,37 +16,30 @@ PARAMS = {
     'appid': API_key,
     'units': 'metric',
     'cnt': 6,
-} 
-
+}
 # d_url = f"https://api.openweathermap.org/data/2.5/weather?lat=10.296758&lon=124.789370&appid={API_key}"
 d_url = f"https://api.openweathermap.org/data/2.5/weather?"
 d_url_5_3 = f"https://api.openweathermap.org/data/2.5/forecast"
 
-
-# response = requests.get(url=d_url, params=PARAMS)
 response = requests.get(url=d_url_5_3, params=PARAMS)
 response.raise_for_status()
-# print(response.status_code)
 data = response.json()
 
 class EmailMessage:
 
     def __init__(self):
-        # MY_EMAIL = "omegakonstrukt@gmail.com"
-        # PWD = "hxtf fjvb aext gsvy"
         self.subject = SUBJECT
-        self.messagebody = "self_message"
+        self.messagebody = ""
         # self.messagebody = message()
         self.email_addr = MY_EMAIL
         self.pwd = PWD
 
     def message(self):
-        self.messagebody = "self_message2"
+        # self.messagebody = "self_message2"
         body = ""
         for hour_data in data['list']:
             id = hour_data['weather'][0]['id']
             time = hour_data['dt_txt']
-
             if int(id) < 701:
                 body += f"At {time}, it could rain.\n"
             else:
@@ -55,15 +47,12 @@ class EmailMessage:
         
         self.messagebody = body
         print(self.messagebody)
-        print(type(self.messagebody))
-        # return body
         
     def send_mail(self):
         self.message()
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(user=self.email_addr, password=self.pwd)
-            # connection.sendmail(from_addr=self.email_addr, to_addrs='laopasana@outlook.com', msg=f"Subject: {self.subject}\nI hope...\n{self.messagebody}") #
             connection.sendmail(from_addr=self.email_addr, to_addrs='laopasana@outlook.com', msg=f"Subject: {self.subject}\n\n{self.messagebody}") #
             connection.quit()
             print('Success')

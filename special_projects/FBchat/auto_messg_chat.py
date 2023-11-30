40dd73f0164595b12ee505357a4a9542eb25e555
Erick <flpasana@outlook.com>
2023-11-30 16:58:02 +0800

    Start of Quotes Project

--- special_projects/FBchat/auto_messg_chat.py
+++ special_projects/FBchat/auto_messg_chat.py
@@ -1,8 +1,8 @@
-# import fbchat
-# from getpass import getpass
-
+import datetime as dt
+import random
 import fbchat
 from getpass import getpass
+# import smtplib
 
 username = "brodpete13@gmail.com"
 client = fbchat.Client(username, getpass())
@@ -23,3 +23,32 @@ if sent:
 # sent = client.send(friend.uid, msg)
 # if sent:
 #     print("Message sent successfully!")
+
+
+#x--------------------------------- Constants ---------------------------------------x
+
+
+#x------------------------------- Get Message Body -----------------------------------x
+
+
+#x--------------------------------- Send Message -----------------------------------x
+now = dt.datetime.now()
+weekday = now.strftime("%A")
+# year = now.year
+# month = now.month
+# # weekday = now.weekday()
+
+if weekday == 'Tuesday':
+    index = random.randint(0, 102)
+    with open('quotes.txt', 'r') as file:
+        quotes = file.readlines()
+        quote = random.choice(quotes)
+        email_body = quote
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PWD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="laopasana@outlook.com", msg=f"Subject: Monday Inspirational Quote\n\n{quote}")
+
+    print(weekday)
+    print(email_body)

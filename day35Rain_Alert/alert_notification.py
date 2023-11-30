d0ae91f10c63ba6709cd22bd333d3a57c7d1ee4e
Erick <flpasana@outlook.com>
2023-11-30 09:06:46 +0800

    email body displayed

--- day35Rain_Alert/alert_notification.py
+++ day35Rain_Alert/alert_notification.py
@@ -1,20 +1,69 @@
 # Python Email/Message Sending
 
 import smtplib
+import requests
+import json
+import datetime
+
+SUBJECT = "Weather Alert!!!"
+MY_EMAIL = "omegakonstrukt@gmail.com"
+PWD = "hxtf fjvb aext gsvy"
+LAT = 10.296758
+LON = 124.789370
+API_key = '3e5771e5877ff5e32e79a8a089ea546d'
+PARAMS = {
+    "lat": LAT,
+    "lon": LON,
+    'appid': API_key,
+    'units': 'metric',
+    'cnt': 6,
+} 
+
+# d_url = f"https://api.openweathermap.org/data/2.5/weather?lat=10.296758&lon=124.789370&appid={API_key}"
+d_url = f"https://api.openweathermap.org/data/2.5/weather?"
+d_url_5_3 = f"https://api.openweathermap.org/data/2.5/forecast"
+
+
+# response = requests.get(url=d_url, params=PARAMS)
+response = requests.get(url=d_url_5_3, params=PARAMS)
+response.raise_for_status()
+# print(response.status_code)
+data = response.json()
 
 class EmailMessage:
 
-    def __init__(self, subject, messagebody):
-        MY_EMAIL = "omegakonstrukt@gmail.com"
-        PWD = "hxtf fjvb aext gsvy"
-        self.subject = subject
-        self.messagebody = messagebody
+    def __init__(self):
+        # MY_EMAIL = "omegakonstrukt@gmail.com"
+        # PWD = "hxtf fjvb aext gsvy"
+        self.subject = SUBJECT
+        self.messagebody = "self_message"
+        # self.messagebody = message()
         self.email_addr = MY_EMAIL
         self.pwd = PWD
+
+    def message(self):
+        self.messagebody = "self_message2"
+        body = ""
+        for hour_data in data['list']:
+            id = hour_data['weather'][0]['id']
+            time = hour_data['dt_txt']
+
+            if int(id) < 701:
+                body += f"At {time}, it could rain.\n"
+            else:
+                body += f"At {time}, could be sunny.\n"
+        
+        self.messagebody = body
+        print(self.messagebody)
+        print(type(self.messagebody))
+        # return body
         
     def send_mail(self):
+        self.message()
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(user=self.email_addr, password=self.pwd)
-            connection.sendmail(from_addr=self.email_addr, to_addrs='laopasana@outlook.com', msg=f"Subject: {self.subject}\n{self.messagebody}")
-            print('Succeess')
+            # connection.sendmail(from_addr=self.email_addr, to_addrs='laopasana@outlook.com', msg=f"Subject: {self.subject}\nI hope...\n{self.messagebody}") #
+            connection.sendmail(from_addr=self.email_addr, to_addrs='laopasana@outlook.com', msg=f"Subject: {self.subject}\n\n{self.messagebody}") #
+            connection.quit()
+            print('Success')
--- day35Rain_Alert/main.py
+++ day35Rain_Alert/main.py
@@ -3,50 +3,53 @@ import json
 import datetime
 from alert_notification import EmailMessage
 
-# send_class = EmailMessage()
-# send_alert = send_class.send_mail()
-LAT = 10.296758
-LON = 124.789370
-API_key = '3e5771e5877ff5e32e79a8a089ea546d'
-PARAMS = {
-    "lat": LAT,
-    "lon": LON,
-    'appid': API_key,
-    'units': 'metric',
-    'cnt': 6,
-} 
-
-# d_url = f"https://api.openweathermap.org/data/2.5/weather?lat=10.296758&lon=124.789370&appid={API_key}"
-d_url = f"https://api.openweathermap.org/data/2.5/weather?"
-d_url_5_3 = f"https://api.openweathermap.org/data/2.5/forecast"
+send_class = EmailMessage()
+# # send_alert = send_class.send_mail()
+# print(message())
+send_class.send_mail()
 
 
-# response = requests.get(url=d_url, params=PARAMS)
-response = requests.get(url=d_url_5_3, params=PARAMS)
-response.raise_for_status()
-# print(response.status_code)
-data = response.json()
-subject = "Weather Alert!!!"
+# SUBJECT = "Weather Alert!!!"
+# LAT = 10.296758
+# LON = 124.789370
+# API_key = '3e5771e5877ff5e32e79a8a089ea546d'
+# PARAMS = {
+#     "lat": LAT,
+#     "lon": LON,
+#     'appid': API_key,
+#     'units': 'metric',
+#     'cnt': 6,
+# } 
 
-def message():
-    body = ""
-    for hour_data in data['list']:
-        # id = hour_data['weather'][0]['id']
-        id = hour_data['weather'][0]['id']
-        time = hour_data['dt_txt']
+# # d_url = f"https://api.openweathermap.org/data/2.5/weather?lat=10.296758&lon=124.789370&appid={API_key}"
+# d_url = f"https://api.openweathermap.org/data/2.5/weather?"
+# d_url_5_3 = f"https://api.openweathermap.org/data/2.5/forecast"
 
-        if int(id) < 701:
-        # if id < 701:
-            body += f"At {time}, it could rain.\n"
-            # print(body)
-            # send_class
-        else:
-            body += f"At {time}, could be sunny.\n"
-            # print(body)
-            # send_class
-    return body
 
-print(message())
+# # response = requests.get(url=d_url, params=PARAMS)
+# response = requests.get(url=d_url_5_3, params=PARAMS)
+# response.raise_for_status()
+# # print(response.status_code)
+# data = response.json()
+
+# def message():
+#     body = ""
+#     for hour_data in data['list']:
+#         # id = hour_data['weather'][0]['id']
+#         id = hour_data['weather'][0]['id']
+#         time = hour_data['dt_txt']
+
+#         if int(id) < 701:
+#         # if id < 701:
+#             body += f"At {time}, it could rain.\n"
+#             # print(body)
+#             # send_class
+#         else:
+#             body += f"At {time}, could be sunny.\n"
+#             # print(body)
+#             # send_class
+#     return print(body)
+
 
 
 # bring_or_not = json.dumps(data['list'][2]['weather'][0]['id'], indent=4)

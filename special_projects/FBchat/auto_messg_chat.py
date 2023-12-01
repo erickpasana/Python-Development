6c5095aaa0cb2269f2441698c1572b2171443035
Erick <flpasana@outlook.com>
2023-12-01 08:30:52 +0800

    start stocks project

--- special_projects/FBchat/auto_messg_chat.py
+++ special_projects/FBchat/auto_messg_chat.py
@@ -4,15 +4,24 @@ import fbchat
 from getpass import getpass
 # import smtplib
 
+#x--------------------------------- Constants ---------------------------------------x
 username = "brodpete13@gmail.com"
-client = fbchat.Client(username, getpass())
+PWD = 'UF3PyGBQxX4u'
+# client = fbchat.Client(username, getpass())
+client = fbchat.Client(username, PWD)
 name = "Frederick Pasana"
-friends = client.getUsers(name)
-friend = friends[0]
-msg = "Hello"
-sent = client.send(friend.uid, msg)
-if sent:
-    print("Message sent successfully!")
+users = client.getUsers(name) # returns a list of matching users
+# user = users[0] # get the first user from the list
+user = name # get the first user from the list
+print(user.uid) # print the user ID
+# pip install fbchat==1.9.7
+# message = 'john : this is a test'
+# inputArray = message.split(':')
+# # 0=username  1=message
+# username = inputArray[0].strip()
+# message = inputArray[1].strip()
+# print(username)
+# print(message)
 
 # username = input("Username: ")
 # client = fbchat.Client(username, getpass())
@@ -24,31 +33,32 @@ if sent:
 # if sent:
 #     print("Message sent successfully!")
 
-
-#x--------------------------------- Constants ---------------------------------------x
-
-
 #x------------------------------- Get Message Body -----------------------------------x
-
+# friends = client.getUsers(name)
+# friend = friends[0]
+# msg = "Hello"
+# sent = client.send(friend.uid, msg)
+# if sent:
+#     print("Message sent successfully!")
 
 #x--------------------------------- Send Message -----------------------------------x
-now = dt.datetime.now()
-weekday = now.strftime("%A")
-# year = now.year
-# month = now.month
-# # weekday = now.weekday()
+# now = dt.datetime.now()
+# weekday = now.strftime("%A")
+# # year = now.year
+# # month = now.month
+# # # weekday = now.weekday()
 
-if weekday == 'Tuesday':
-    index = random.randint(0, 102)
-    with open('quotes.txt', 'r') as file:
-        quotes = file.readlines()
-        quote = random.choice(quotes)
-        email_body = quote
+# if weekday == 'Tuesday':
+#     index = random.randint(0, 102)
+#     with open('quotes.txt', 'r') as file:
+#         quotes = file.readlines()
+#         quote = random.choice(quotes)
+#         email_body = quote
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=PWD)
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs="laopasana@outlook.com", msg=f"Subject: Monday Inspirational Quote\n\n{quote}")
+#     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+#         connection.starttls()
+#         connection.login(user=MY_EMAIL, password=PWD)
+#         connection.sendmail(from_addr=MY_EMAIL, to_addrs="laopasana@outlook.com", msg=f"Subject: Monday Inspirational Quote\n\n{quote}")
 
-    print(weekday)
-    print(email_body)
+#     print(weekday)
+#     print(email_body)

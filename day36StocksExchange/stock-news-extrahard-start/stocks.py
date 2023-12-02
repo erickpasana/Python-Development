44b87975f599bd63e38f872090b5f16632de9dc6
Erick <flpasana@outlook.com>
2023-12-03 07:16:12 +0800

    Waiting for stocks API to renew

--- day36StocksExchange/stock-news-extrahard-start/stocks.py
+++ day36StocksExchange/stock-news-extrahard-start/stocks.py
@@ -2,7 +2,7 @@ import requests
 import json
 import datetime
 
-percentage_change = 0
+# percentage_change = 0
 
 class Stocks:
 
@@ -26,63 +26,64 @@ class Stocks:
         }
 
         try:
-            stocks_response = requests.get(url=self.STOCKS_URL, params=stocks_params)
-            stocks_response.raise_for_status()
-            stocks_data = stocks_response.json()
-            # Get the latest data from the time series
+            stock_response = requests.get(url=self.STOCKS_URL, params=stocks_params)
+            stock_response.raise_for_status()
+            stocks_data = stock_response.json()
             time_series = stocks_data["Time Series (60min)"]
+            # time_series = stocks_data['interval']["60min"]
             latest_time = max(time_series.keys())
             latest_data = time_series[latest_time]
             # Get the latest price, percentage change, and volume of the stock
             latest_price = float(latest_data["4. close"])
             previous_price = float(time_series[sorted(time_series.keys())[-2]]["4. close"])
-            percentage_change = round(((latest_price - previous_price) / previous_price) * 100, 2)
-            volume = int(latest_data["5. volume"])
+            self.percentage_change = round(((latest_price - previous_price) / (previous_price)) * 100, 2)
+            # percentage_change = round(((latest_price - previous_price) / (previous_price)) * 100, 2)
+            # volume = int(latest_data["5. volume"])
+            self.volume = int(latest_data["5. volume"])
             # Print the results
-            print(f"The latest price of {self.STOCK} is ${latest_price}")
-            print(f"The percentage change of {self.STOCK} is {percentage_change}%")
-            print(f"The volume of {self.STOCK} is {volume}")
+            # percentage = (f"The percentage change of {self.STOCK} is {self.percentage_change}%")
+            vol = f"The volume of {self.STOCK} is {self.volume}"
+            if self.percentage_change > 0:
+                self.ARROW = ARROW_UP
+            else:
+                self.ARROW = ARROW_DOWN
+            # print(self.percentage_change)
+            # return f"The latest price of {self.STOCK} is ${latest_price}\n{self.percentage}\n{vol}"
+            # return stocks_data
+
         except Exception as e:
             print(f"An error occurred: {e}")
 
-
-        self.percentage_change = 90
-        print(self.percentage_change)
-        print(stocks_data)
+            # return f"An error occurred: {e}"
 
 
-
-
+        # self.percentage_change = 90
+        # print(self.percentage_change)
+        # print(stocks_data)
 
 
 
-        # # try:
-        # stock_response = requests.get(url=self.STOCKS_URL, params=stocks_params)
-        # stock_response.raise_for_status()
-        # stocks_data = stock_response.json()
-        # time_series = stocks_data["Time Series (60min)"]
-        # # time_series = stocks_data['interval']["60min"]
-        # latest_time = max(time_series.keys())
-        # latest_data = time_series[latest_time]
-        # # Get the latest price, percentage change, and volume of the stock
-        # latest_price = float(latest_data["4. close"])
-        # previous_price = float(time_series[sorted(time_series.keys())[-2]]["4. close"])
-        # self.percentage_change = round(((latest_price - previous_price) / (previous_price)) * 100, 2)
-        # percentage_change = round(((latest_price - previous_price) / (previous_price)) * 100, 2)
-        # print(percentage_change)
-        # volume = int(latest_data["5. volume"])
-        # self.volume = int(latest_data["5. volume"])
-        # # Print the results
-        # # percentage = (f"The percentage change of {self.STOCK} is {self.percentage_change}%")
-        # vol = f"The volume of {self.STOCK} is {volume}"
-        # # if percentage_change > 0:
-        # #     self.ARROW = ARROW_UP
-        # # else:
-        # #     self.ARROW = ARROW_DOWN
-        # # return f"The latest price of {self.STOCK} is ${latest_price}\n{percentage}\n{vol}"
-        # # return time_series
 
-        # # except Exception as e:
 
-        # # print(f"An error occurred: {e}")
-        # # return f"An error occurred: {e}"
+
+
+
+        # try:
+        #     stocks_response = requests.get(url=self.STOCKS_URL, params=stocks_params)
+        #     stocks_response.raise_for_status()
+        #     stocks_data = stocks_response.json()
+        #     # Get the latest data from the time series
+        #     time_series = stocks_data["Time Series (60min)"]
+        #     latest_time = max(time_series.keys())
+        #     latest_data = time_series[latest_time]
+        #     # Get the latest price, percentage change, and volume of the stock
+        #     latest_price = float(latest_data["4. close"])
+        #     previous_price = float(time_series[sorted(time_series.keys())[-2]]["4. close"])
+        #     percentage_change = round(((latest_price - previous_price) / previous_price) * 100, 2)
+        #     volume = int(latest_data["5. volume"])
+        #     # Print the results
+        #     print(f"The latest price of {self.STOCK} is ${latest_price}")
+        #     print(f"The percentage change of {self.STOCK} is {percentage_change}%")
+        #     print(f"The volume of {self.STOCK} is {volume}")
+        # except Exception as e:
+        #     print(f"An error occurred: {e}")

5ddc39fe873a4d3b6b3b5627b98ef01bb0d484c0
Erick <flpasana@outlook.com>
2023-12-05 08:01:41 +0800

    Done Day 36

--- day36StocksExchange/stock-news-extrahard-start/SendMessage.py
+++ day36StocksExchange/stock-news-extrahard-start/SendMessage.py
@@ -1,12 +1,11 @@
 import smtplib
-import requests
-import json
-import datetime
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class Message:
 
 # x-------------------- Attributes --------------------------x
-    def __init__(self, subject, body):
+    def __init__(self, subject, body, ):   #Arrow
         self.MY_EMAIL = "omegakonstrukt@gmail.com"
         self.PWD = "hxtf fjvb aext gsvy"
         self.SUBJECT = subject
@@ -14,9 +13,56 @@ class Message:
 
 # x-------------------- Send Message -------------------------------x
     def sendMessage(self):
+        msg = MIMEMultipart()
+        msg['Subject'] = self.SUBJECT
+        msg['From'] = self.MY_EMAIL
+        msg['To'] = 'laopasana@outlook.com'
+        text = self.BODY
+        part1 = MIMEText(text, 'plain', 'utf-8')
+        msg.attach(part1)
+
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(user=self.MY_EMAIL, password=self.PWD)
-            # connection.sendmail(from_addr=MY_EMAIL, to_addrs='laopasana@outlook.com', msg=f"Subject: TSLA Stocks\n\n{headline}\n{news_body}")
-            connection.sendmail(from_addr=self.MY_EMAIL, to_addrs='laopasana@outlook.com', msg=f"Subject: {self.SUBJECT}\n\n{self.BODY}")      #\n\n{news_body1}
+            # connection.sendmail(from_addr=self.MY_EMAIL, to_addrs='laopasana@outlook.com', msg=f"Subject: {self.SUBJECT}\n\n{self.BODY}")
+            connection.send_message(msg)
             print('Success')
+
+
+
+
+
+
+            
+    # def sendMessage(self):
+    #     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    #         connection.starttls()
+    #         connection.login(user=self.MY_EMAIL, password=self.PWD)
+    #         # connection.sendmail(from_addr=MY_EMAIL, to_addrs='laopasana@outlook.com', msg=f"Subject: TSLA Stocks\n\n{headline}\n{news_body}")
+    #         connection.sendmail(from_addr=self.MY_EMAIL, to_addrs='laopasana@outlook.com', msg=f"Subject: {self.SUBJECT}\n\n{self.BODY}")      #\n\n{news_body1}
+    #         print('Success')
+
+# ARROW = None
+# ARROW_UP = "🔺"
+# ARROW_DOWN = "🔻"
+# if percent_latest_closing_diff > 0:
+#     ARROW = ARROW_UP
+# else:
+#     ARROW = ARROW_DOWN  
+
+# Create the container email message.
+# msg = MIMEMultipart()
+# msg['Subject'] = 'Subject with emoji 📧'
+# msg['From'] = 'your_email@example.com'
+# msg['To'] = 'recipient_email@example.com'
+
+# # Create a text/plain message with UTF-8 encoding for emojis.
+# text = 'Hello, this is a message with an emoji 👋'
+# part1 = MIMEText(text, 'plain', 'utf-8')
+
+# # Attach parts into message container.
+# msg.attach(part1)
+
+# # Send the email via your SMTP server.
+# with smtplib.SMTP('smtp.example.com') as server:
+#     server.send_message(msg)
--- day36StocksExchange/stock-news-extrahard-start/news.py
+++ day36StocksExchange/stock-news-extrahard-start/news.py
@@ -10,27 +10,30 @@ class News:
         self.COMPANY_NAME = "Tesla Inc"
         self.title = ""
         self.description = ""
+        # self.articles = news_data['articles']
 
     def news(self):
         NEWS_URL = "https://newsapi.org/v2/everything?"
         NEWS_API_KEY = "115ef21aaf38432fa21925fdc32ffddf"
         news_params = {
-            'q': self.COMPANY_NAME,
+            'qInTitle': self.COMPANY_NAME,
+            'apiKey': NEWS_API_KEY,
+            # 'q': self.COMPANY_NAME,
             # 'from': '2023-12-10',
             # 'from': '',
-            'from': '2023-12-01',
-            'to': '2023-12-01',
-            'sortBy': 'publishedAt',
-            'apiKey': NEWS_API_KEY,
+            # 'from': '2023-12-01',
+            # 'to': '2023-12-01',
+            # 'sortBy': 'publishedAt',
         }
             
         news_response = requests.get(url=NEWS_URL, params=news_params)
-        # news_response = requests.get(url=NEWS_URL)
         news_response.raise_for_status()
-        stocks_data = news_response.json()
-        self.title = stocks_data['articles'][0]['title']
-        self.description = stocks_data['articles'][0]['description']
-        # print(f"Status: {stocks_data['status']}   Total Results: {stocks_data['totalResults']}")
+        news_data = news_response.json()
+        self.title = news_data['articles'][0]['title']
+        self.description = news_data['articles'][0]['description']
+        self.articles = news_data['articles'][:3]
+        # self.articles = json.dumps(self.articles, indent=4)
+        formatted_articles = [f"Headline: {article['title']}\nBrief: {article['description']}" for article in self.articles]
         # return f"Status: {stocks_data['status']}  Total News: {stocks_data['totalResults']}"
         # return f"Article: {stocks_data['articles'][0]['description']}\n{stocks_data['articles'][0]['title']}"
-        # return stocks_data
+        return formatted_articles

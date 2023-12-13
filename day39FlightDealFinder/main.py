8b8371e709c747f91f2d069f550784e59257a0de
Erick <flpasana@outlook.com>
2023-12-13 15:43:17 +0800

    Day 39

---
+++ day39FlightDealFinder/main.py
@@ -0,0 +1,3 @@
+onedrive_url = "https://consystest-my.sharepoint.com/:x:/g/personal/flpasana_consys_biz/ETqaNOstDxtFslE_dRbnpD0BPKuBf6uqH1UPv7-1M4gWxw?e=0lcZg8"
+MS_Graph_Client_ID = "e5b3d4cb-ea71-4b58-9dc9-5339633fa8ac"
+# a7a8b943-4b6f-437a-8185-9411d8bc977a

a35bdcfffb92abfe3465e285313b234a240b8568
Erick <flpasana@outlook.com>
2023-12-15 19:04:56 +0800

    Day 39 in flight search

--- day39FlightDealFinder/flight-deals-start/flight_search.py
+++ day39FlightDealFinder/flight-deals-start/flight_search.py
@@ -1,5 +1,38 @@
+import os
+import requests
+import json
+from flight_data import FlightData
+
+search_endpoint = 'https://api.tequila.kiwi.com/v2/search'
+# data[0]['price']
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    
-    def __init__(self):
-        self.IATA_code = "Testing"
+    def __init__(self,):
+        self.endpoint = search_endpoint
+        self.fly_from = 'city:LON'
+        
+        self.date_from='16/12/2024'
+        self.date_to='03/05/2024' 
+        self.headers = {
+            # 'apikey': self.kiwi_API,
+            'apikey': '2z6qJ-wObDBvJ93qovMh7TQRi246n1gx',
+            'Content-Type': 'application/json',
+        }
+
+    def price(self):
+        endpoint_to_use = f"{self.endpoint}?{self.fly_from}&{self.date_from}&{self.date_to}"
+        print(endpoint_to_use)
+        response = requests.get(endpoint_to_use, headers=self.headers)
+        print(response.text)
+        # get_price = response.json()
+        # print(json.dumps(get_price, indent=4))#'data'[0]['price']
+
+        def get_id(self):
+            # self.request_body = {
+            #     'term': self.city
+            # }
+            flight_response = requests.get(self.kiwi_endpoint, params=self.request_body, headers=self.headers)
+            # print(flight_response.text)
+            flight_data = flight_response.json()
+            return flight_data['locations'][0]['code']
+            # return flight_data['locations'][0]['id']
--- day39FlightDealFinder/flight-deals-start/main.py
+++ day39FlightDealFinder/flight-deals-start/main.py
@@ -4,10 +4,37 @@ import requests
 from data_manager import DataManager
 from pprint import pprint
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 #x------------------------------- Sheety Requests ------------------------------------x
-sheety = DataManager()
+sheety = DataManager('id', 'flight_code')
 # sheet_data = sheety.add_row()
-flight_code = FlightSearch()
 
-print(sheety.edit_row_data())
+# iata_code = flight_code.get_code()
+
+# print(sheety.edit_row_data())
+# £
+# response = requests.get(sheety.end_point)
+# data = response.json()
+# for i, row in enumerate(data['prices']):
+#     flight_code = FlightData(row['city'])
+#     row_id = row['id']
+#     print(f"{row['city']}: £{0}")
+
+#x------------------------------- Flight Search ------------------------------------x
+fls = FlightSearch()
+# fls.price()
+
+
+    # edit_IATA = requests.put(f"{self.end_point}/{row_id}", json=request_body, headers=self.sheety_header)
+    # print(edit_IATA.text)
+
+    # request_body = {
+    #     "price": {
+    #         # 'city': 'Manila',
+    #         "iataCode": flight_code.get_code(),
+    #         # "lowestPrice": 350,
+    #     }
+    # }
+    # sheety.edit_row_data()
+    # sheety = DataManager(row['id'], flight_code.get_code())

0e84949eadfcbd55272355f36ebedd4b740af941
Erick <flpasana@outlook.com>
2023-12-16 09:39:10 +0800

    Day 39 print City&price

--- day39FlightDealFinder/flight-deals-start/main.py
+++ day39FlightDealFinder/flight-deals-start/main.py
@@ -6,28 +6,34 @@ from pprint import pprint
 from flight_search import FlightSearch
 from flight_data import FlightData
 
+pound_sign = '£'
+
 #x------------------------------- Sheety Requests ------------------------------------x
 sheety = DataManager('id', 'flight_code')
-# sheet_data = sheety.add_row()
-
-# iata_code = flight_code.get_code()
-
-# print(sheety.edit_row_data())
-# £
-# response = requests.get(sheety.end_point)
-# data = response.json()
-# for i, row in enumerate(data['prices']):
-#     flight_code = FlightData(row['city'])
-#     row_id = row['id']
-#     print(f"{row['city']}: £{0}")
-
-#x------------------------------- Flight Search ------------------------------------x
-fls = FlightSearch()
-# fls.price()
-
-
+response = requests.get(sheety.end_point)
+data = response.json()
+for i, row in enumerate(data['prices']):
+    city_from = 'LON'
+    city_to = row['iataCode']
+    flight_price = FlightSearch(city_from, city_to)
+    flight_price.get_price()
+    #flight_data.price
+    flight_data = FlightData()
+    print(f"{row['city']}: £{flight_price.get_price()}")
+    # print(f"{row['city']}: £{flight_data.price}")
     # edit_IATA = requests.put(f"{self.end_point}/{row_id}", json=request_body, headers=self.sheety_header)
     # print(edit_IATA.text)
+
+#x------------------------------- Flight Search ------------------------------------x
+# fls = FlightSearch()
+# print(fls.get_price())
+# fl_dt_price = FlightData()
+# print(fl_dt_price.price)
+#x------------------------------- Flight Search ------------------------------------x
+
+
+
+
 
     # request_body = {
     #     "price": {

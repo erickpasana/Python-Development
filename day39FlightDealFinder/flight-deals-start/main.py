67c5bf6609adbeec3d233abb0d6e7dbeed9cc5a3
Erick <flpasana@outlook.com>
2023-12-16 13:25:35 +0800

    Day 39 done

--- day39FlightDealFinder/flight-deals-start/main.py
+++ day39FlightDealFinder/flight-deals-start/main.py
@@ -12,23 +12,58 @@ pound_sign = '£'
 sheety = DataManager('id', 'flight_code')
 response = requests.get(sheety.end_point)
 data = response.json()
+
+# ----------------------print City: Price--------------------x
+# for i, row in enumerate(data['prices']):
+#     city_from = 'LON'
+#     city_to = row['iataCode']
+#     flight_price = FlightSearch(city_from, city_to)
+#     flight_price.get_price()
+#     #flight_data.price
+#     flight_data = FlightData()
+#     print(f"{row['city']}: £{flight_price.get_price()}")
+
+#x------------------------------- Notification ------------------------------------x
+import sys
+msg_path = r"C:\Users\flpas\OneDrive - Connection Systems Development\Python-Development"
+sys.path.insert(0, msg_path)#\SendMessage.py
+from SendMessage import Message
+subject = 'Cheap Flights'
+email_list = ['laopasana@outlook.com', ]#'dodj_ortalla@yahoo.com'
+
 for i, row in enumerate(data['prices']):
     city_from = 'LON'
     city_to = row['iataCode']
     flight_price = FlightSearch(city_from, city_to)
-    flight_price.get_price()
-    #flight_data.price
-    flight_data = FlightData()
-    print(f"{row['city']}: £{flight_price.get_price()}")
-    # print(f"{row['city']}: £{flight_data.price}")
-    # edit_IATA = requests.put(f"{self.end_point}/{row_id}", json=request_body, headers=self.sheety_header)
-    # print(edit_IATA.text)
+    floor_price = row['lowestPrice']
+    actual_price = flight_price.get_price()
+    body = f"""
+    Low price alert! Only ${actual_price} to fly
+    from Manila to {row['city']}."""
+    print(f"{floor_price} : {actual_price}")
+    if actual_price < floor_price:
+        email_msg = Message(subject, body, email_list)
+        email_msg.sendMessage()
 
-#x------------------------------- Flight Search ------------------------------------x
-# fls = FlightSearch()
-# print(fls.get_price())
-# fl_dt_price = FlightData()
-# print(fl_dt_price.price)
+
+"""
+Price
+
+Departure City Name
+
+Departure Airport IATA Code
+
+Arrival City Name
+
+Arrival Airport IATA Code
+
+Outbound Date
+
+Inbound Date
+"""
+
+
+    # flight_data = FlightData()
 #x------------------------------- Flight Search ------------------------------------x
 
 

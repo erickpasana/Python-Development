8ad3fc0e743f094fd68f72d66837cc1d714bbbeb
Erick <flpasana@outlook.com>
2023-12-18 13:41:56 +0800

    Done using dataframe in Quotes

--- special_projects/QUOTES/main.py
+++ special_projects/QUOTES/main.py
@@ -1,24 +1,33 @@
 import datetime as dt
 import random
+import pandas as pd
+import numpy as np
 from SendMessage import Message
-from data_list import quotes_list
+# from data_list import quotes_list
 
+# index = random.randint(0, len(quotes_list)-1)
+# quote = quotes_list[index]
 #x--------------------------------- Constants ---------------------------------------x
 
 #x------------------------------- Get Message Body -----------------------------------x
-index = random.randint(0, len(quotes_list)-1)
-quote = quotes_list[index]
+data = pd.read_csv('data_list.csv')
+q_index = random.randint(0, len(data)+1)
+quote = f"{data.loc[q_index, 'Quote']} - {data.loc[q_index, 'Author']}"
 
 #x--------------------------------- Send Message -----------------------------------x
 subject = "Daily Quotes"
 body = quote
-email_list = ['laopasana@outlook.com', 'dodj_ortalla@yahoo.com']
+email_list = ['laopasana@outlook.com', ]#'dodj_ortalla@yahoo.com'
 send_msg = Message(subject, body, email_list)
 send_msg.sendMessage()
 
+#x--------------------------------- Run/Debug -----------------------------------x
 
 
 
+print(quote)
+# print(data.loc[53, 'Quote'])
+# print(data.iloc[53, 0])
 # print(quote)
 # print(len(quotes_list))
 # print(quotes_list[53])

0d27df2c88c0ad4cdc4ed7b843727ce0fecb61a8
Erick <flpasana@outlook.com>
2024-01-08 16:56:09 +0800

    Day 47 done

--- day47ScrapeShoppee/main.py
+++ day47ScrapeShoppee/main.py
@@ -1,27 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import lxml
 
-# url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
-url = "https://shopee.ph/V380-Outdoor-CCTV-Dual-Camera-Wifi-Connect-To-Cellphone-With-Voice-Dual-Lens-Waterproof-Night-Visio-i.102443022.20790600795?xptdk=bf906563-14d1-4089-af20-f7dc9c864f236"
-# header = {
-#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
-#     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
-# }
+url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
-product_price = soup.select("span")   #div.c-_44qnta 
+response = requests.get(url, headers=header)
+soup = BeautifulSoup(response.content, "lxml")
+price = soup.find(class_="a-offscreen").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+
+# url = "https://shopee.ph/V380-Outdoor-CCTV-Dual-Camera-Wifi-Connect-To-Cellphone-With-Voice-Dual-Lens-Waterproof-Night-Visio-i.102443022.20790600795?xptdk=bf906563-14d1-4089-af20-f7dc9c864f236"
+# product_price = soup.select("span")   #div.c-_44qnta 
+# print(soup.prettify())
 # product_price = soup.select("div.c-pqTWkA")[0]
 # product_price = soup.select("h1.c-rqONlU")
 # product_price = soup.find('div.c-flex items-center', {'class': 'pqTWkA'})
 
-print(product_price)
-
-# print(soup.prettify())
-
-#{} div.pqTWkA    [, 'p'], 
-
-# price = soup.find(class_="a-offscreen").get_text()
-# price_without_currency = price.split("$")[1]
-# price_as_float = float(price_without_currency)
-# print(price_as_float)

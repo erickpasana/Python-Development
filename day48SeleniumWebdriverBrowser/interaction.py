9c2854830a11f214091bec3361028f9fc14284a9
Erick <flpasana@outlook.com>
2024-01-19 09:11:21 +0800

    Day 48 click search icon first before typing

--- day48SeleniumWebdriverBrowser/interaction.py
+++ day48SeleniumWebdriverBrowser/interaction.py
@@ -1,8 +1,10 @@
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 url = "https://en.wikipedia.org/wiki/Main_Page"
@@ -11,8 +13,31 @@ edge_option.add_experimental_option('detach', True)
 service = Service(executable_path=r"C:\Users\flpas\AppData\Local\Programs\msedgedriver.exe")
 driver = webdriver.Edge(service=service, options=edge_option)
 driver.get(url)
+#----------------------------- Click Link ------------------------------x
+# link_click = driver.find_element(By.LINK_TEXT, 'Mark Baldwin')
+# link_click.click()
 
-widget_ = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
+#----------------------------- Type Search ------------------------------x
+wait = WebDriverWait(driver, 10)
+# sleep(5)
+# element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "vector-icon")))
+element = driver.find_element(By.CSS_SELECTOR, "a.cdx-button--fake-button--enabled span.vector-icon")
+element.click()
+# actions = ActionChains(driver)
+# actions.move_to_element(element).perform()
+search_ = driver.find_element(By.NAME, value="search")
+wait = WebDriverWait(driver, 10)
+search_.send_keys("Python", Keys.ENTER)
 
-print(widget_.text)
-driver.quit()
+
+# print(element.text)
+# driver.quit()
+
+
+
+# button_ = driver.find_element(By.CSS_SELECTOR, 'span vector-icon mw-ui-icon-search mw-ui-icon-wikimedia-search')
+# button_.click()
+# sleep(5)
+# wait = WebDriverWait(driver, 10)
+# print(widget_.text)
+# widget_ = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
--- day48SeleniumWebdriverBrowser/main.py
+++ day48SeleniumWebdriverBrowser/main.py
@@ -10,15 +10,15 @@ service = Service(executable_path=r"C:\Users\flpas\AppData\Local\Programs\msedge
 driver = webdriver.Edge(service=service, options=edge_option)
 driver.get(url)
 
-button_ = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Dual Lens CCTV+64G']")
-button_.click()
-sleep(10)
+# button_ = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Dual Lens CCTV+64G']")
+# button_.click()
+# sleep(10)
 # item_price = driver.find_element(By.XPATH, value="//*[@id='main']/div/div[2]/div[1]/div[1]/div/div/section[1]/section[2]/div/div[3]/div[2]/div/section/div/div[2]/div[1]")
-# item_price = driver.find_element(By.CSS_SELECTOR, "div.G27FPf")
+item_price = driver.find_element(By.CSS_SELECTOR, "div.G27FPf")
 
-# print(item_price.text)
+print(item_price.text)
 # driver.close()
-driver.quit()
+# driver.quit()
 
 
 
